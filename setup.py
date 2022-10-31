cb184b0dd52ce74e835072fe9b8404f64084a1c4
Sabuat Urbina <sabuaturbina@yahoo.com>
2022-10-31 15:00:52 -0300

    correccion de problemas

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open ("requirements.txt") as f:
-    requirements = f.read().splitlines()
+with open ("requisitos.txt") as f:
+    requisitos= f.read().splitlines()
 
 setup(
     name="operaciones",
@@ -14,7 +14,7 @@ setup(
     description="A pacage with two basic math operations",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sabuat/package-operaciones",
-    install_requires=requirements,
+    url="https://github.com/sabuat/package-operaciones.git",
+    install_requires=requisitos,
     python_requires=">=3.8",
 )
